344a990bc7f6047d28dfd0b99e45c2effec81940
金汉生 <jinhansheng@gonglu.info>
2025-06-30 11:09:30 +0800

    向量点积

--- src/pages/VectorDot/index.tsx
+++ src/pages/VectorDot/index.tsx
@@ -2,7 +2,7 @@ import { useRef, useEffect } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import mesh from "./mesh";
-// import mesh2 from "./mesh2
+import mesh2, { normals } from "./mesh2";
 function VectorDot() {
   const mount = useRef<HTMLDivElement>(null);
   const scene = new THREE.Scene();
@@ -10,7 +10,7 @@ function VectorDot() {
   useEffect(() => {
     {
       scene.add(mesh);
-      // scene.add(mesh2);
+      scene.add(mesh2);
 
       const directionLight = new THREE.DirectionalLight(0xffffff, 2);
       directionLight.position.set(500, 400, 300);
@@ -35,6 +35,16 @@ function VectorDot() {
       function render() {
         renderer.render(scene, camera);
         requestAnimationFrame(render);
+
+        const dir = camera.getWorldDirection(new THREE.Vector3());
+        normals.forEach((normal, index) => {
+          // 判断是否钝角或锐角
+          if (dir.dot(normal) > 0) {
+            mesh2.children[index].visible = false;
+          } else {
+            mesh2.children[index].visible = true;
+          }
+        });
       }
 
       render();
@@ -45,6 +55,16 @@ function VectorDot() {
 
   return (
     <>
+      <div className="group relative">
+        <button className="absolute z-990 top-0 cursor-pointer bg-green-500 text-white px-4 py-2 rounded">
+          知识点
+        </button>
+        <div className="hidden group-hover:block absolute top-full left-0 mt-0 bg-gray-700 text-white p-3 rounded-lg shadow-xl">
+          向量 Vector3 的 dot 方法可以计算点积，当点积大于 0，夹角是锐角，反之是钝角。 用 camera.getWorldDirection
+          可以拿到 camera 的方向向量，用它和墙的法线向量做点积，就可以判断夹角是否是锐角，从而决定隐藏、显示。
+          <p>dir.dot(dir2) &lt; 0 ? '钝角' : '锐角'</p>
+        </div>
+      </div>
       <div ref={mount} style={{ width: "100%", height: "100%" }} />
     </>
   );
--- src/pages/VectorDot/mesh.ts
+++ src/pages/VectorDot/mesh.ts
@@ -10,7 +10,7 @@ const origin = new THREE.Vector3(0, 0, 0);
 const dir = new THREE.Vector3(1, 0, 0);
 dir.normalize();
 
-const ArrowHelper = new THREE.ArrowHelper(dir, origin, 4, 'red');
+const ArrowHelper = new THREE.ArrowHelper(dir, origin, 400, 'red');
 group.add(ArrowHelper);
 
 const dir2 = new THREE.Vector3(-1, 2, 0);
@@ -19,5 +19,8 @@ dir2.normalize();
 const ArrowHelper2 = new THREE.ArrowHelper(dir2, origin, 400, 'blue');
 
 group.add(ArrowHelper2);
+
+// 点积相乘
+// console.log(dir.dot(dir2) < 0 ? '钝角' : '锐角')
 
 export default group;
