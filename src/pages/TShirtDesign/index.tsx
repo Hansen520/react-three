004fa02730ff1e8be69866bbd6d7743f0d4a9a0f
金汉生 <jinhansheng@gonglu.info>
2025-07-11 17:29:27 +0800

    保存图片或视频

--- src/pages/TShirtDesign/index.tsx
+++ src/pages/TShirtDesign/index.tsx
@@ -7,7 +7,8 @@ import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import mesh from "./mesh";
 import { DecalGeometry } from "three/examples/jsm/Addons.js";
-import { ColorPicker, Radio } from "antd";
+import { Button, ColorPicker, Radio } from "antd";
+import gsap from "gsap";
 // import mesh2 from "./mesh2";
 
 function TShirtDesign() {
@@ -16,6 +17,8 @@ function TShirtDesign() {
 
   const changeTShirtColorRef = useRef<any>("");
   const changeTextureRef = useRef<any>("");
+  const downloadImgRef = useRef<any>("");
+  const downloadVideoRef = useRef<any>("");
 
   useEffect(() => {
     {
@@ -67,7 +70,7 @@ function TShirtDesign() {
       // const texture = loader.load("/heart.png");
       // texture.colorSpace = THREE.SRGBColorSpace;
 
-      let texture = null;
+      let texture: any = null;
 
       renderer.domElement.addEventListener("click", (e) => {
         const y = -((e.offsetY / height) * 2 - 1);
@@ -98,24 +101,74 @@ function TShirtDesign() {
           scene.add(mesh);
         }
       });
+      // 改变颜色
+      function changeTShirtColor(color: string) {
+        const tshirt: any = scene.getObjectByName("tshirt");
+        if (tshirt) {
+          tshirt.material.color.set(color);
+        }
+      }
 
       // 改变贴花
       function changeTexture(url: string) {
         texture = loader.load(url);
         texture.colorSpace = THREE.SRGBColorSpace;
       }
+
+      // 下载图片
+      function downloadBlob(blob: any, filename: string) {
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        URL.revokeObjectURL(url);
+      }
+
+      // 转成图片
+      function downloadImg() {
+        renderer.render(scene, camera);
+        // 转成blob
+        renderer.domElement.toBlob((blob) => {
+          if (blob) {
+            downloadBlob(blob, "design.png");
+          }
+        }, "image/png");
+      }
+
+      // 下载视频
+      function downloadVideo() {
+        // 转成流,renderer.domElement = domElement
+        const stream = renderer.domElement.captureStream(60);
+        const recorder = new MediaRecorder(stream);
+        recorder.start();
+        recorder.ondataavailable = (event) => {
+          if (event.data.size > 0) {
+            downloadBlob(event.data, `design.webm`);
+          }
+        };
+        // setTimeout(() => {
+        //   recorder.stop();
+        // }, 3000);
+        gsap.to(scene.rotation, {
+          y: 2 * Math.PI,
+          duration: 3,
+          onComplete() {
+            scene.rotation.y = 0;
+            recorder.stop();
+          }
+        })
+      }
+
       // 初始化函数赋值
       changeTShirtColorRef.current = changeTShirtColor;
       changeTextureRef.current = changeTexture;
+      downloadImgRef.current = downloadImg;
+      downloadVideoRef.current = downloadVideo;
     }
   }, []);
-
-  function changeTShirtColor(color: string) {
-    const tshirt: any = scene.getObjectByName("tshirt");
-    if (tshirt) {
-      tshirt.material.color.set(color);
-    }
-  }
 
   return (
     <>
@@ -165,6 +218,12 @@ function TShirtDesign() {
               </Radio>
             </Radio.Group>
           </div>
+        </div>
+        <div className="ope-item" style={{ display: "flex", alignItems: "center" }}>
+          <Button type="primary" onClick={() => downloadImgRef.current()}>
+            保存为图片
+          </Button>
+          <Button type="default" onClick={() => downloadVideoRef.current()}>保存为视频</Button>
         </div>
       </div>
       <div ref={mount} style={{ width: "100%", height: "100%" }} />
